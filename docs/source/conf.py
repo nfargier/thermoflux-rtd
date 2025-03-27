62e22433ad80719176fdf216a51dca5170448e4c
Nathan Fargier <133126094+nfargier@users.noreply.github.com>
2025-03-27 11:35:39 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Thermo-flux'
+copyright = '2024, E.N.Smith'
+author = 'E.N.Smith'
 
 release = '0.1'
 version = '0.1.0'
